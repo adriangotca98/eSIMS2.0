7c5c7f54fc22782277fa484c10f7d268372c5cec
IrinaApostol <48645231+IrinaApostol@users.noreply.github.com>
2019-12-02 00:00:49 +0200

    Add files via upload

---
+++ eSims/eSims/Controllers/ProfessorsController.cs
@@ -0,0 +1,72 @@
+﻿using eSims.Models;
+using eSims.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+namespace eSims.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorsController : ControllerBase
+    {
+        private readonly ProfessorService _professorService;
+        public ProfessorsController(ProfessorService professorService)
+        {
+            _professorService = professorService;
+        }
+        [HttpGet]
+        public ActionResult<List<Professor>> Get() =>
+            _professorService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetProfessor")]
+        public ActionResult<Professor> Get(string id)
+        {
+            var professor = _professorService.Get(id);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return professor;
+        }
+
+        [HttpPost]
+        public ActionResult<Professor> Create(Professor professor)
+        {
+            _professorService.Create(professor);
+
+            return CreatedAtRoute("GetProfessor", new { id = professor.Id.ToString() }, professor);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Professor professorIn)
+        {
+            var professor = _professorService.Get(id);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            _professorService.Update(id, professorIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var professor = _professorService.Get(id);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            _professorService.Remove(professor.Id);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ eSims/eSimsTest/ProfessorTest.cs
@@ -0,0 +1,20 @@
+using eSims.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eSimsTest
+{
+    [TestClass]
+    public class ProfessorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arange
+            
+            //Act
+            
+            //Assert
+            
+        }
+    }
+}
