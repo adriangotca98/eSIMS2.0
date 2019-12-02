e9377475bd4a85960f9c34e30172f2960b464fdc
LarisaBardita <48147940+LarisaBardita@users.noreply.github.com>
2019-12-02 09:47:05 +0200

    Add files via upload

---
+++ eSims1/eSims1/Services/SubjectsService.cs
@@ -0,0 +1,44 @@
+﻿using eSims1.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eSims1.Services
+{
+    public class SubjectsService
+    {
+        private readonly IMongoCollection<Subjects> _subjects;
+
+        public SubjectsService(IeSimsDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _subjects = database.GetCollection<Subjects>(settings.SubjectsCollectionName);
+        }
+
+        public List<Subjects> Get() =>
+            _subjects.Find(subject => true).ToList();
+
+        public Subjects Get(string id) =>
+            _subjects.Find<Subjects>(subject => subject.Id == id).FirstOrDefault();
+
+        public Subjects Create(Subjects subject)
+        {
+            _subjects.InsertOne(subject);
+            return subject;
+        }
+
+        public void Update(string id, Subjects subjectIn) =>
+            _subjects.ReplaceOne(subjects => subjects.Id == id, subjectIn);
+
+        public void Remove(Subjects subjectIn) =>
+            _subjects.DeleteOne(subjects => subjects.Id == subjectIn.Id);
+
+        public void Remove(string id) =>
+            _subjects.DeleteOne(subjects => subjects.Id == id);
+
+
+
+    }
+}
---
+++ eSims1/eSims1Test/SubjectsControllerTest.cs
@@ -0,0 +1,21 @@
+using eSims1.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eSims1Test
+{
+    [TestClass]
+    public class SubjectsControllerTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            //Arange
+           
+
+            //Act
+            
+
+
+        }
+    }
+}
