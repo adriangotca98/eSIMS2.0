d3b2061c2981f62e82e7a6257fc33ba3043e0863
Adrian Gotca <adriangotca98@gmail.com>
2019-12-17 00:32:07 +0200

    removed JsonSerialization as we don't need it anymore

--- eSims/eSims/Models/Student.cs
+++ eSims/eSims/Models/Student.cs
@@ -11,25 +11,18 @@ namespace eSims.Models
 		[BsonRepresentation(BsonType.ObjectId)]
 		public string Id;
 		[BsonElement("Registration Number")]
-		[JsonProperty("Registration Number")]
 		public string RegistrationNumber;
 		[BsonElement("First Name")]
-		[JsonProperty("First Name")]
 		public string FirstName;
 		[BsonElement("Last Name")]
-		[JsonProperty("Last Name")]
 		public string LastName;
 		[BsonElement("Year")]
-		[JsonProperty("Year")]
 		public int Year;
 		[BsonElement("Group")]
-		[JsonProperty("Group")]
 		public string Group;
 		[BsonElement("Subjects")]
-		[JsonProperty("Subjects")]
 		public List<string> Subjects;
 		[BsonElement("GradeIDs")]
-		[JsonProperty("GradeIDs")]
 		public List<string> GradeIDs;
 	}
 }
