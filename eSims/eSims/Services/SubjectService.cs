06c0a3a069cb8b7f64d811f75b4300428459317d
LarisaBardita <barditalarisa99@gmail.com>
2019-12-27 15:22:21 +0200

    Addressed #22

--- eSims/eSims/Services/SubjectService.cs
+++ eSims/eSims/Services/SubjectService.cs
@@ -9,7 +9,6 @@ namespace eSims.Services
 	public class SubjectService : ISubjectService
 	{
 		private readonly IMongoCollection<Subject> _subjects;
-
         private readonly IMongoCollection<Professor> _professors;
         public SubjectService(IeSimsDatabaseSettings settings)
 		{
@@ -21,29 +20,47 @@ namespace eSims.Services
         }
 		public List<Subject> Get() =>
 			_subjects.Find(subject => true).ToList();
-		public Subject Get(string id) =>
-			_subjects.Find<Subject>(subject => subject.Id == id).FirstOrDefault();
+        public Subject Get(string id) =>
+            FindSubjectById(id);
 		public Subject Create(Subject subject)
 		{
-            if (Get(subject.Id) != null)
+            if (FindSubjectById(subject.Id) != null || VerifyProfessors(subject) == false)
             {
                 return null;
-            }
-            foreach (string _professor in subject.ProfessorIds.ToList())
-            {
-                if (_professors.Find(professor => professor.Id == _professor).FirstOrDefault() == null)
-                {
-                    return null;
-                }
             }
             _subjects.InsertOne(subject);
 			return subject;
 		}
-		public void Update(string id, Subject subjectIn) =>
-			_subjects.ReplaceOne(subjects => subjects.Id == id, subjectIn);
+
+		public bool Update(string id, Subject subjectIn)
+        {
+            if(VerifyProfessors(subject) == false)
+            {
+                return false;
+            }
+
+        }
+			
+
 		public void Remove(Subject subjectIn) =>
 			_subjects.DeleteOne(subjects => subjects.Id == subjectIn.Id);
 		public void Remove(string id) =>
 			_subjects.DeleteOne(subjects => subjects.Id == id);
-	}
+
+        private Subject FindSubjectById(string id) =>
+            _subjects.Find(subject => subject.Id == id).FirstOrDefault();
+
+        private bool VerifyProfessors(Subject subject)
+        {
+            foreach (string _professor in subject.ProfessorIds.ToList())
+            {
+                if (_professors.Find(professor => professor.Id == _professor).FirstOrDefault() == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
 }
